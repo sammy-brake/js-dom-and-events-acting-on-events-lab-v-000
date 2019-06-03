93247e6395f5b3cfee3dabe1f736d8f372d7ebb4
Samantha brake <sammy.brake@gmail.com>
2019-06-03 01:41:11 +0000

    Done.

--- index.js
+++ index.js
@@ -12,13 +12,23 @@ function retrieveEmployeeInformation(){
 
 
 function addNewElementAsLi() {
-
+  let newEmployee = retrieveEmployeeInformation();
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${newEmployee}</li>`)
 };
 
 function addNewLiOnClick() {
+  let submit = document.querySelector('input[type="submit"]');
+  submit.addEventListener('click', function(event){
+    addNewElementAsLi();
+    document.querySelector('input').value = "";
+  })
 
 };
 
 function clearEmployeeListOnLinkClick() {
-
+  let link = document.querySelector('a');
+  let ul = document.querySelector('ul');
+  link.addEventListener('click', function(event){
+    ul.innerHTML = '';
+  })
 };
